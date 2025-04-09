2e9f951bc2258a44e543d500b77f39c389a4dd00
JoseG1297 <mordo1309@gmail.com>
2025-04-09 16:47:47 -0600

    insertar registros

--- comandos.bat
+++ comandos.bat
@@ -1,4 +1,7 @@
 @echo off
 echo "Ejecutando comandos ..."
-awslocal dynamodb create-table --table-name usuarios --attribute-definitions AttributeName=usuarioId,AttributeType=S --key-schema AttributeName=usuarioId,KeyType=HASH --provisioned-throughput ReadCapacityUnits=1,WriteCapacityUnits=1 
+REM  awslocal dynamodb create-table --table-name usuarios --attribute-definitions AttributeName=usuarioId,AttributeType=S --key-schema AttributeName=usuarioId,KeyType=HASH --provisioned-throughput ReadCapacityUnits=1,WriteCapacityUnits=1 
 echo "Tabla usuarios creada."
+set "JSON_FILE=itemsDynamodb.json"
+awslocal dynamodb put-item --table-name usuarios --item file://%JSON_FILE%
+echo "Items insertados en la tabla usuarios."
