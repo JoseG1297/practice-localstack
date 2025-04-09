44a9da0ac31a7ccd88b97b7028153056688536e5
JoseG1297 <mordo1309@gmail.com>
2025-04-09 17:01:01 -0600

    CRUD dynamoDB

--- comandos.bat
+++ comandos.bat
@@ -5,3 +5,12 @@ echo "Tabla usuarios creada."
 set "JSON_FILE=itemsDynamodb.json"
 awslocal dynamodb put-item --table-name usuarios --item file://%JSON_FILE%
 echo "Items insertados en la tabla usuarios."
+
+awslocal dynamodb get-item --table-name usuarios --key '{"usuarioId": {"S": "1"}}'
+echo "Item con usuarioId 1 obtenido."
+
+awslocal dynamodb update-item --table-name usuarios --key '{"usuarioId": {"S": "1"}}' --update-expression "SET nombre = :n" --expression-attribute-values '{":n": {"S": "Juan"}}'
+echo "Item con usuarioId 1 actualizado."
+
+awslocal dynamodb delete-item --table-name usuarios --key '{"usuarioId": {"S": "1"}}'
+echo "Item con usuarioId 1 eliminado."
